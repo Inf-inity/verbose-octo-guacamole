df832ddcd1a2d4dc32f8e247eeef0bde39f71c07
Inf-inity <83883849+Inf-inity@users.noreply.github.com>
2023-02-11 09:36:54 +0100

    changed docstring

--- app/core/version.py
+++ app/core/version.py
@@ -4,7 +4,7 @@ from .environment import SHOW_COMMIT, VERSION
 
 
 def get_version() -> str:
-    """returns the current version "`v.<major>.<minor>.<patch>-<last_commit>`" """
+    """returns the current version "`v.<major>.<minor>.<patch>-<last_commit>@<current_branch>`" """
     if SHOW_COMMIT:
         return f"v.{VERSION}-{getoutput('git describe --tags --always')}@{getoutput('git branch --show-current')}"
     else:
